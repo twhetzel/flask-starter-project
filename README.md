bf745e92a21d548d7d9d6e96fcfb529f0500de4d
Trish Whetzel <plwhetzel@gmail.com>
2017-11-17 10:59:47 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# flask-starter-project
+# Flask Starter Project
+Basic template for a Flask web site.
+
+## Usage
+- Prepare your environment by executing the requirements.txt file, e.g. `pip install -r requirements.txt`
+- Run the web app as `python app.py`
+
+
+
---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect, jsonify, \
+    url_for, flash
+
+# from sqlalchemy import create_engine, asc, desc, \
+#     func, distinct
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.serializer import loads, dumps
+
+# from database_setup import Base, Things
+
+import random
+import string
+import logging
+import json
+import httplib2
+import requests
+
+
+app = Flask(__name__)
+
+
+# Connect to database and create database session
+# engine = create_engine('sqlite:///flaskstarter.db')
+# Base.metadata.bind = engine
+
+# DBSession = sessionmaker(bind=engine)
+# session = DBSession()
+
+
+# Display all things
+@app.route('/')
+def showMain():
+    things = ["thing1", "thing2", "cat-in-the-hat"]
+
+    return render_template('things.html', things=things)
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
